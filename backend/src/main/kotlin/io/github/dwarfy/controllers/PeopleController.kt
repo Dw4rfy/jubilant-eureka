a1933b13759644e91261f694c67b294d7bbda9b9
dwarfy <dwarfy@yahoo.com>
2023-03-12 12:10:29 +0100

    cleanup of warnings

--- backend/src/main/kotlin/io/github/dwarfy/controllers/PeopleController.kt
+++ backend/src/main/kotlin/io/github/dwarfy/controllers/PeopleController.kt
@@ -10,36 +10,36 @@ import org.springframework.web.bind.annotation.*
 @RestController
 @RequestMapping("/api/people")
 @CrossOrigin(origins = ["http://localhost:5173"])
-public class PeopleController(val peopleRepository: PeopleRepository) {
+class PeopleController(private val peopleRepository: PeopleRepository) {
     init {
-        peopleRepository.create("Joacim", 34);
+        peopleRepository.create("Joacim", 34)
         peopleRepository.create("Christoffer", 28)
     }
 
     @GetMapping
-    public fun getList(): ResponseEntity<Collection<Person>> {
+    fun getList(): ResponseEntity<Collection<Person>> {
         return ResponseEntity(peopleRepository.getListOfPeople(), HttpStatus.OK)
     }
 
     @PostMapping
-    public fun create(@RequestBody input: PersonRequest): ResponseEntity<Person> {
-        if (input.name.isNullOrEmpty() || input.age <= 0) {
+    fun create(@RequestBody input: PersonRequest): ResponseEntity<Person> {
+        if (input.name.isEmpty() || input.age <= 0) {
             return ResponseEntity(HttpStatus.BAD_REQUEST)
         }
 
-        var person = peopleRepository.create(input.name, input.age)
+        val person = peopleRepository.create(input.name, input.age)
         return ResponseEntity(person, HttpStatus.CREATED)
     }
 
     @PutMapping("{id}")
-    public fun update(@PathVariable id: Int, @RequestBody data: PersonRequest): ResponseEntity<Person> {
-        var person = peopleRepository.update(id, data)
+    fun update(@PathVariable id: Int, @RequestBody data: PersonRequest): ResponseEntity<Person> {
+        val person = peopleRepository.update(id, data)
             ?: return ResponseEntity(HttpStatus.NOT_FOUND)
         return ResponseEntity(person, HttpStatus.OK)
     }
 
     @DeleteMapping("{id}")
-    public fun delete(@PathVariable id: Int): ResponseEntity<String> {
+    fun delete(@PathVariable id: Int): ResponseEntity<String> {
         if (!peopleRepository.delete(id))
             return ResponseEntity(HttpStatus.NOT_FOUND)
         return ResponseEntity(HttpStatus.NO_CONTENT)
